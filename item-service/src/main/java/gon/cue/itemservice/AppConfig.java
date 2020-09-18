88602e98f8d6e7d05fbc3587debc8d2dfb88ff57
David Gonzalez <david.eve.za@gmail.com>
2020-09-18 14:08:09 -0500

    Create AppConfig.java
    Creating bean for providing a way for connecting with other services

---
+++ item-service/src/main/java/gon/cue/itemservice/AppConfig.java
@@ -0,0 +1,13 @@
+package gon.cue.itemservice;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
+public class AppConfig {
+    @Bean("restClient")
+    public RestTemplate registerRestTemplate(){
+        return new RestTemplate();
+    }
+}
