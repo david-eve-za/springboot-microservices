416afff14fe86b047155d36eb0e9d3ae1bdc4bd6
David Gonzalez <david.eve.za@gmail.com>
2020-09-18 14:33:03 -0500

    Implementing RestTemplate

---
+++ item-service/src/main/java/gon/cue/itemservice/controllers/ItemController.java
@@ -0,0 +1,26 @@
+package gon.cue.itemservice.controllers;
+
+import gon.cue.itemservice.models.Item;
+import gon.cue.itemservice.services.ItemService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class ItemController {
+    @Autowired
+    private ItemService itemService;
+
+    @GetMapping("/list")
+    public List<Item> list(){
+        return itemService.findAll();
+    }
+
+    @GetMapping("/detail/{id}/amount/{amount}")
+    public Item detail(@PathVariable Long id,@PathVariable Integer amount){
+        return itemService.findById(id, amount);
+    }
+}
--- item-service/src/main/java/gon/cue/itemservice/services/ItemServiceImpl.java
+++ item-service/src/main/java/gon/cue/itemservice/services/ItemServiceImpl.java
@@ -1,24 +1,36 @@
 package gon.cue.itemservice.services;
 
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
+import gon.cue.itemservice.models.Product;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import gon.cue.itemservice.models.Item;
+import org.springframework.web.client.RestTemplate;
 
 @Service
 public class ItemServiceImpl implements ItemService {
 
-	@Override
-	public List<Item> findAll() {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    @Autowired
+    private RestTemplate restTemplate;
 
-	@Override
-	public Item findById(Long id, Integer amount) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    @Override
+    public List<Item> findAll() {
+        List<Product> products = Arrays.asList(restTemplate.getForObject("http://localhost:8001/list", Product[].class));
+        return products.stream().map(product -> new Item(product, 1)).collect(Collectors.toList());
+    }
+
+    @Override
+    public Item findById(Long id, Integer amount) {
+        Map<String, String> pathVariables = new HashMap<>();
+        pathVariables.put("id", id.toString());
+        Product product = restTemplate.getForObject("http://localhost:8001/detail/{id}", Product.class, pathVariables);
+        return new Item(product, amount);
+    }
 
 }
